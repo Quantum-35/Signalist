99676843c4a60fbe32d513f46f70ca29dff61abf
Quantum-35 <quantumcomputing35@icloud.com>
2026-02-07 10:06:31 +0300

    implement headers

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import NavItems from './NavItems'
+import { User } from 'lucide-react'
+import UserDropdown from './UserDropdown'
+
+const Header: () => React.ReactNode = () => {
+  return (
+    <header className='sticky top-0 header'>
+        <div className='container header-wrapper'>
+            <Link href="/">
+                <Image 
+                    src="/assets/icons/logo.svg" 
+                    alt="Signalist Logo"
+                    width={150} 
+                    height={50} 
+                    className='h-8 w-auto cursor-pointer'/>
+            </Link>
+            <nav className='hidden sm:block'>
+                <NavItems />
+            </nav>
+            <UserDropdown />
+        </div>
+    </header>
+  )
+}
+
+export default Header
